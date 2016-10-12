4d660404a4c5050f08c58569688da8fc7248cc49
Angus Griffith <16sn6uv@gmail.com>
2016-10-12 08:30:27 -0700

    store 64bit hash

--- core/expression.h
+++ core/expression.h
@@ -7,7 +7,7 @@
 typedef struct {
     BaseExpression base;
     uint64_t last_evaluated;
-    uint32_t hash;
+    uint64_t hash;
     uint32_t argc;
     BaseExpression* head;
     BaseExpression* leaves[];  // zero length array of pointers
