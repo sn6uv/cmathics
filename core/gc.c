360b6c56be720796b1c9a9ed8abcb1e2ba2f15cf
Angus Griffith <16sn6uv@gmail.com>
2016-10-11 22:24:37 -0700

    basic gc

---
+++ core/gc.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#include "int.h"
+#include "real.h"
+#include "rational.h"
+#include "expression.h"
+#include "definitions.h"
+#include "string.h"
+#include "gc.h"
+
+
+// decrements the reference by one and frees if unreferenced
+void decref_free(BaseExpression* expr) {
+    expr->ref--;
+    assert(expr->ref >= 0);
+    if (expr->ref == 0) {
+        free(expr);
+    }
+}
+
+
+void BaseExpression_free(BaseExpression* expr) {
+    switch (expr->type) {
+        case MachineIntegerType:
+            MachineInteger_free((MachineInteger*) expr);
+            break;
+        case BigIntegerType:
+            BigInteger_free((BigInteger*) expr);
+            break;
+        case MachineRealType:
+            MachineReal_free((MachineReal*) expr);
+            break;
+        case BigRealType:
+            BigReal_free((BigReal*) expr);
+            break;
+        case RationalType:
+            Rational_free((Rational*) expr);
+            break;
+        // case ComplexType:
+        //     Complex_free((Complex*) expr);
+        //     break;
+        case ExpressionType:
+            Expression_free((Expression*) expr);
+            break;
+        // case SymbolType:
+        //     Symbol_free((Symbol*) expr);
+        //     break;
+        case StringType:
+            String_free((String*) expr);
+            break;
+        // case RawExpressionType:
+        //     RawExpression_free((RawExpression*) expr);
+        //     break;
+        default:
+            assert(false);
+    }
+}
---
+++ core/gc.h
@@ -0,0 +1,6 @@
+#ifndef GC_H
+#define GC_H
+#include "types.h"
+
+void decref_free(BaseExpression* expr);
+#endif
